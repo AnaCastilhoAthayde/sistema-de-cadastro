2d75c4ffa43c865909d0a3f4af688d0642f53bed
Ana Luiza Castilho Athayde <castilho.athayde@gmail.com>
2025-06-05 15:08:24 -0300

    Add files via upload

--- cadastro.py
+++ cadastro.py
@@ -1,3 +1,26 @@
+def cadastrar_nome(cadastro):
+    novo_nome = input("Digite o nome da Pessoa:" )
+    cadastro.append(novo_nome)
+    print(f"Usuário {novo_nome} foi adicionado com sucesso")
+
+def lista_nomes(cadastro):
+        print("\nLista de nomes cadastrados:")
+        for i, nome in enumerate(cadastro,start=1):
+            print(f" {i}. {nome}")
+
+def excluir_cadastro(cadastro):         
+    excluir_nome = input("Digite o nome para excluir: ")
+    if excluir_nome in cadastro:
+         cadastro.remove(excluir_nome)
+    print(f"{excluir_nome} foi removido.")
+
+def nao_encontrado(cadastro):
+      print("Nome não encontrado.")
+
+def sair_do_cadastro(cadastro):
+     print("Saindo...")
+
+
 def menu():
     cadastro = []
     while True:
@@ -9,26 +32,20 @@ def menu():
         opcao = input("Escolha uma opção: ")
 
         if opcao == '1':
-            novo_nome = input("Digite o nome da Pessoa:" )
-            cadastro.append(novo_nome)
-            print(f"Usuário {novo_nome} foi adicionado com sucesso")
+            cadastrar_nome(cadastro)
         elif opcao == '2':
-            print("\nLista de nomes cadastrados:")
-            for i, nome in enumerate(cadastro, start=1):
-                print(f" {i}. {nome}")
+            lista_nomes(cadastro)
+            
         elif opcao =='3':
-            excluir_nome = input("Digite o nome para excluir: ")
-            if excluir_nome in cadastro:
-                  cadastro.remove(excluir_nome)
-                  print(f"{excluir_nome} foi removido.")
-            else:
-                print("Nome não encontrado.")
+            excluir_cadastro(cadastro)
+     
         elif opcao == '0':
-            print("Sainda...")
-            break
+            sair_do_cadastro
+                 
         else:
-            print("!!!!! OPÇÃO INVÁLIDA.TENTE NOVAMNETE. !!!!!")
+            print("!!!!! Opção Inválida. Tente Novamente!!!!!")
 
-#Chamada de funcao
+
+#Chamda de funcao
 menu()
       
